5390bc27dae6702ba868debd3e8a4b116a811767
D-M4N <56065975+D-M4N@users.noreply.github.com>
2019-11-27 13:52:40 +0200

    Login xml upload

--- SimpleAppSln/SimpleApp/SimpleApp/LogInArea.xaml.cs
+++ SimpleAppSln/SimpleApp/SimpleApp/LogInArea.xaml.cs
@@ -3,28 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace SimpleApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ActivityOne : ContentPage
+    public partial class LogInArea : ContentPage
     {
-
-        public ActivityOne()
+        public LogInArea()
         {
             InitializeComponent();
-
-            
         }
 
-        private void LoginButton_Clicked(object sender, EventArgs e)
+        private void OnLogin_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new LogInArea());
+            Navigation.PushAsync(new Contents());
         }
-
-      
-
     }
 }
